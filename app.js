684c5abab90cd32ba26b8baca96af031cabb4e55
Kavita Pathak <pathakkavita273@gmail.com>
2025-08-02 16:51:38 +0530

    feat: Retrieve tasks

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const tasks = [
+  {
+    id: 2,
+    title: "Create a new project",
+    description: "Create a new project using Magic",
+    completed: false,
+  },
+];
+
 app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
 });
 
-
+app.get("/api/v1/tasks", (req, res) => {
+  res.send(tasks);
+});
 
 module.exports = app;
